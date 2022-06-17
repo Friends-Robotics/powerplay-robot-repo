c849b7c00750a00fc2cf1b25a6ec032fbdf6abaf
Phyrik <buchananscc@gmail.com>
2022-06-17 12:11:28 +0100

    finish instruction list version of autonomous op mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousopmodes/BlueShippingHubAutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousopmodes/BlueShippingHubAutonomousOpMode.java
@@ -35,6 +35,9 @@ public class BlueShippingHubAutonomousOpMode extends LinearOpMode {
         autonomousMovements.add(new TurnDegreesAutonomousMovement(teamHardwareMap, 90, 0.5, TurningDirection.Right));
         autonomousMovements.add(new MoveForwardInchesAutonomousMovement(teamHardwareMap, 27, 0.5));
         while (opModeIsActive()) {
+            teamHardwareMap.leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            teamHardwareMap.rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
             if (lastStageFinished == 0) {
                 if (timer.milliseconds() < 500) { // raise arm for start of autonomous to get over pipes
                     teamHardwareMap.hexMotor1.setPower(0.5);
@@ -46,14 +49,19 @@ public class BlueShippingHubAutonomousOpMode extends LinearOpMode {
 
             if (lastStageFinished == numOfPreStages) {
                 AutonomousMovement currentAutonomousMovement = autonomousMovements.get(lastStageFinished - numOfPreStages);
+                boolean finished = false;
                 if (currentAutonomousMovement instanceof MoveForwardInchesAutonomousMovement) {
-                    boolean finished = AutonomousMethods.MoveForwardInches(currentAutonomousMovement.teamHardwareMap, ((MoveForwardInchesAutonomousMovement) currentAutonomousMovement).inches, ((MoveForwardInchesAutonomousMovement) currentAutonomousMovement).speed);
-                    if (finished) {
-                        lastStageFinished++;
-                    }
+                    MoveForwardInchesAutonomousMovement moveForwardInchesAutonomousMovement = (MoveForwardInchesAutonomousMovement) currentAutonomousMovement;
+                    finished = AutonomousMethods.MoveForwardInches(currentAutonomousMovement.teamHardwareMap, moveForwardInchesAutonomousMovement.inches, moveForwardInchesAutonomousMovement.speed);
                 }
                 else if (currentAutonomousMovement instanceof TurnDegreesAutonomousMovement) {
-
+                    TurnDegreesAutonomousMovement turnDegreesAutonomousMovement = (TurnDegreesAutonomousMovement) currentAutonomousMovement;
+                    finished = AutonomousMethods.TurnDegrees(currentAutonomousMovement.teamHardwareMap, turnDegreesAutonomousMovement.degrees, turnDegreesAutonomousMovement.speed, turnDegreesAutonomousMovement.turningDirection);
+                }
+                if (finished) {
+                    teamHardwareMap.leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    teamHardwareMap.rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    lastStageFinished++;
                 }
             }
         }
